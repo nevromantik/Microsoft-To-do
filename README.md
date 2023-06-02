b9e054c5abc0680776f696f3a1785c0f8566e894
nevromantik <jessicamoretti55@gmail.com>
2023-06-02 09:07:42 +0200

    navbar category fixed

--- README.md
+++ README.md
@@ -4,3 +4,17 @@ json-server db.json --routes routes.json
 
 1. Utilizzo il server json solo per controllare l'autenticazione, ma la manipolazione dei todos avviene separatamente in quanto json server non permette di accedere agli elementi innestati
 perciò la mia soluzione è stata quella di utilizzare json server per l'autenticazione, currentUser viene salvato in uno stato e i todos vengono manipolati con useReducer. 
+
+
+
+ setCurrentUser((prev) => {
+      const newCategoryObj = {
+        title: newCategory,
+        icon: "HiOutlineMenu" // Inserisci qui l'icona corretta per il nuovo elemento della categoria
+      };
+  
+      return {
+        ...prev,
+        customCategory: [...prev.customCategory, newCategoryObj]
+      };
+    });
--- src/Components/Category/Category.js
+++ src/Components/Category/Category.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import style from "./category.module.css";
 import { BsSun } from "react-icons/bs";
@@ -9,10 +9,12 @@ import { FiHome } from "react-icons/fi";
 import { HiOutlineMenu } from "react-icons/hi";
 import { AppContext } from "../../App";
 import { useContext } from "react";
-import axios from 'axios'
-function Category() {
-  const { allUsers, setCurrentUser, currentUser } = useContext(AppContext);
+import axios from "axios";
+import uniqid from "uniqid";
+function Categorytest() {
 
+  const { setCurrentUser, currentUser } = useContext(AppContext);
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [defaultCategory, setDefaultCategory] = useState([
     { title: "La mia giornata", icon: BsSun },
     { title: "Importante", icon: AiOutlineStar },
@@ -20,54 +22,123 @@ function Category() {
     { title: "Assegnate a me", icon: BiUser },
     { title: "Attività", icon: FiHome },
   ]);
-  
-  const [newCategory, setNewCategory] = useState(''); 
 
-  
+  const [newCatTitle, setNewCatTitle] = useState("");
+  const [catId, setCatId] = useState("");
+
   const handleAddCategory = () => {
     setCurrentUser((prev) => {
       const newCategoryObj = {
-        title: newCategory,
-        icon: HiOutlineMenu 
+        title: "Nuovo elenco",
+        icon: HiOutlineMenu,
+        id: uniqid(),
       };
+  
+      const customCategory = prev.customCategory || []; // Verifica se prev.customCategory è definito come un array
+  
+      const updatedCustomCategory = [...customCategory, newCategoryObj];
   
       return {
         ...prev,
-        customCategory: [...prev.customCategory, newCategoryObj]
+        customCategory: updatedCustomCategory,
       };
-    });     
-    
-  }
+    });
+  };
+  const handleUpdateCategory = (catId) => {
+    //Ho id input category
+    // Ho nuovo title
+
+    const selectedCategory = currentUser?.customCategory?.find((el) => {
+      return el?.id === catId;
+    });
+    selectedCategory.title = newCatTitle;
+    setCurrentUser((prev) => {
+      return {
+        ...prev,
+        customCategory: currentUser.customCategory.map((el) => {
+          if (el.customCategory?.id === selectedCategory?.id) {
+            return { ...el, title: newCatTitle };
+          } else {
+            return el;
+          }
+        }),
+      };
+    });
+  };
+  console.log(currentUser);
+
   return (
     <>
-    <div className={`${style.x} ${style.categoryContainer}`}>
-      <div className={style.defaultcatbox}>
-        {defaultCategory.map((cat, index) => {
+      <div className={`${style.x} ${style.categoryContainer}`}>
+        <div className={style.defaultcatbox}>
+          {defaultCategory.map((cat, index) => {
+            return (
+              <div key={index} className={style.category}>
+                <div className={style.categoryTitle}>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                    className={style.icontitlebox}
+                  >
+                    {
+                      <cat.icon
+                        style={{
+                          fontSize: "1.5rem",
+                          color:
+                            cat.title === "Assegnate a me"
+                              ? "#8ec0aa"
+                              : "#a9a9a9",
+                        }}
+                      />
+                    }
+
+                    <p>{cat.title}</p>
+                  </div>
+
+                  <div className={style.todocounter}>1</div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        {currentUser?.customCategory?.map((cat, index) => {
           return (
-            <div key={index} className={style.category}>
+            <div key={cat.id} className={style.category}>
               <div className={style.categoryTitle}>
-                <div
+                <form
                   style={{
                     display: "flex",
                     justifyContent: "center",
                     alignItems: "center",
                   }}
                   className={style.icontitlebox}
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    handleUpdateCategory(catId);
+                  }}
                 >
                   {
                     <cat.icon
-                      style={{
-                        fontSize: "1.5rem",
-                        color:
-                          cat.title === "Assegnate a me"
-                            ? "#8ec0aa"
-                            : "#a9a9a9",
-                      }}
+                      style={{ fontSize: "1.5rem", color: "#6e80c9" }}
                     />
                   }
-
-                  <p>{cat.title}</p>
-                </div>
+                  <input
+                  className={style.inputTitle}
+                    type="text"
+                    placeholder={cat.title}
+                    style={{ border: "none", backgroundColor: "transparent" }}
+                    data-id={cat.id}
+                    
+                    onChange={(e) => {
+                      const dataId = e.target.getAttribute("data-id");
+                      setCatId(dataId);
+                      setNewCatTitle(e.target.value);
+                    }}
+                  ></input>
+                </form>
 
                 <div className={style.todocounter}>1</div>
               </div>
@@ -75,40 +146,20 @@ function Category() {
           );
         })}
       </div>
-      {currentUser?.customCategory?.map((cat, index) => {
-        return (
-          <div key={index} className={style.category}>
-            <div className={style.categoryTitle}>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-                className={style.icontitlebox}
-              >
-                {<cat.icon style={{ fontSize: "1.5rem", color: "#6e80c9" }} />}
-
-                <p>{cat.title}</p>
-              </div>
-
-              <div className={style.todocounter}>1</div>
-            </div>
-          </div>
-        );
-      })}
-  </div>
-      <form className={style.categoryInput} onSubmit={(e) => {
-        e.preventDefault(); 
-        handleAddCategory()
-      }}>
+      <div className={style.categoryInput}>
         <div className={style.addCategoryBtn}>
-          <button>+</button>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              handleAddCategory();
+            }}
+          >
+            + Nuovo elenco
+          </button>
         </div>
-        <input type="text" placeholder="Nuovo elenco" onChange={(e) => setNewCategory(e.target.value)}/>
-      </form>
+      </div>
     </>
   );
 }
 
-export default Category;
+export default Categorytest;
