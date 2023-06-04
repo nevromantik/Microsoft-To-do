1cc7c4b662d1f3247c420c00194b08bef1afe395
nevromantik <jessicamoretti55@gmail.com>
2023-06-04 09:34:54 +0200

    completed fix2

--- src/Components/Tasks/CompletedTasks.js
+++ src/Components/Tasks/CompletedTasks.js
@@ -1,12 +1,39 @@
 import React from "react";
-import style from "./completed.module.css";
+import style2 from "./completed.module.css";
 import { useContext } from "react";
 import App, { AppContext } from "../../App";
-function CompletedTasks({title, id}) {
+import ContextMenu from "./ContextMenu";
+import { BsCheck } from "react-icons/bs";
+import style from "./task.module.css";
+import { AiOutlineStar } from "react-icons/ai";
+function CompletedTasks({ title, id, category }) {
   return (
-    <div key={id}>
-      <p>{title}</p>
-    </div>
+    <>
+    <div key={id} className={style.task} >
+        <div>
+          <div className={style.check}>
+            {" "}
+            <BsCheck
+              style={{
+                color: "white",
+                position: "relative",
+                left: "1px",
+                top: "1px",
+              }}
+            />
+          </div>
+        </div>
+        <div className={style.infoTodo}>
+          <p>{title}</p>
+
+          <p className={style.todoCategory}>{category}</p>
+        </div>
+        <div className={style.importantIcon}>
+          <AiOutlineStar style={{ fontSize: "1.7rem", color: "#aeafaf", position:'relative', bottom:'0.4rem' }} />
+        </div>
+      </div>
+      
+    </>
   );
 }
 
--- src/Components/Tasks/Tasks.js
+++ src/Components/Tasks/Tasks.js
@@ -5,30 +5,40 @@ import { AppContext } from "../../App";
 import { AiOutlineArrowUp } from "react-icons/ai";
 import { BsArrowsFullscreen } from "react-icons/bs";
 import { BsLightbulb } from "react-icons/bs";
-import uniqid from 'uniqid'
+
+import uniqid from "uniqid";
 import Task from "./Task";
 import Options from "../Options/Options";
 import CompletedTasks from "./CompletedTasks";
+import { IoIosArrowForward, IoIosArrowDown } from "react-icons/io";
 function Tasks() {
-  const { selectedCategory, setSelectedCategory, currentUser, setCurrentUser, selectedCatId, options, setOptions, completed, setCompleted } =
-    useContext(AppContext);
-    const [showDiv, setShowDiv] = useState(false);
-    const [divStyle, setDivStyle] = useState({});
-    const [todoId, setTodoId] = useState('')
-    const handleContextMenu = (event, id) => {
-      event.preventDefault();
-      setShowDiv(true);
-      setDivStyle({ left: event.clientX, top: event.clientY });
-      setTodoId(id)
-    };
-  
-    const handleMouseDown = () => {
-      setShowDiv(false);
-    };
+  const {
+    selectedCategory,
+    setSelectedCategory,
+    currentUser,
+    setCurrentUser,
+    selectedCatId,
+    options,
+    setOptions,
+    completed,
+    setCompleted,
+  } = useContext(AppContext);
+  const [showDiv, setShowDiv] = useState(false);
+  const [divStyle, setDivStyle] = useState({});
+  const [todoId, setTodoId] = useState("");
+  const handleContextMenu = (event, id) => {
+    event.preventDefault();
+    setShowDiv(true);
+    setDivStyle({ left: event.clientX, top: event.clientY });
+    setTodoId(id);
+  };
 
-
+  const handleMouseDown = () => {
+    setShowDiv(false);
+  };
+  const [showCompleted, setShowCompleted] = useState(false);
   const [tasksList, setTasksList] = useState([]);
-  const [newTodoTitle, setNewTodoTitle] = useState('');
+  const [newTodoTitle, setNewTodoTitle] = useState("");
   useEffect(() => {
     const selected = currentUser?.customCategory?.find((el) => {
       return el?.id === selectedCatId;
@@ -40,19 +50,19 @@ function Tasks() {
     );
     setTasksList(filteredTasksList);
   }, [selectedCatId, currentUser, selectedCategory, setSelectedCategory]);
-   
+
   const handleNewTodo = () => {
     const newTodo = {
-        id: uniqid(),
-        title: newTodoTitle,
-        category: selectedCategory.title,
-        completed: false,
-        subTasks: []
-    }
-   setCurrentUser((prev) => {
-    return {...prev, todos: [...prev.todos, newTodo]}
-   });
-  }
+      id: uniqid(),
+      title: newTodoTitle,
+      category: selectedCategory.title,
+      completed: false,
+      subTasks: [],
+    };
+    setCurrentUser((prev) => {
+      return { ...prev, todos: [...prev.todos, newTodo] };
+    });
+  };
   return (
     <div className={style.taskcontainer}>
       <div className={style.taskCategoryDateInfo}>
@@ -66,16 +76,17 @@ function Tasks() {
         <button className={style.showDetailsBtn}>
           <BsLightbulb style={{ fontSize: "1rem" }} />
         </button>
-        <button className={style.changebcBtn} style={{ fontSize: "1rem" }} onClick={(e) => {
-          e.preventDefault();
-          setOptions(!options)
-        }}>
+        <button
+          className={style.changebcBtn}
+          style={{ fontSize: "1rem" }}
+          onClick={(e) => {
+            e.preventDefault();
+            setOptions(!options);
+          }}
+        >
           °°°
         </button>
-        <div>
-        {options ? <Options/> : null}
-        </div>
-        
+        <div>{options ? <Options /> : null}</div>
       </div>
       <div className={style.sortTaskBtn}>
         <button>
@@ -85,31 +96,71 @@ function Tasks() {
           In ordine di importanza
         </button>
       </div>
-      <div className={style.tasksList}         
->
+      <div className={`${style.tasksList} ${style.scroll}`}>
         {tasksList?.map((el) => {
-          return <Task title={el?.title} category={el?.category} id={el?.id} showDiv={showDiv} setShowDiv={setShowDiv} todoId={todoId} 
-          divStyle={divStyle}
-          handleContextMenu={(e) => handleContextMenu(e, el?.id)}
-          handleMouseDown={handleMouseDown} />;
+          return (
+            <Task
+              title={el?.title}
+              category={el?.category}
+              id={el?.id}
+              showDiv={showDiv}
+              setShowDiv={setShowDiv}
+              todoId={todoId}
+              divStyle={divStyle}
+              handleContextMenu={(e) => handleContextMenu(e, el?.id)}
+              handleMouseDown={handleMouseDown}
+            />
+          );
         })}
-      </div>
-      <div className={style.completedTasks}>
-      {completed?.length > 0 && selectedCategory.title === completed[0].category ?
-      
-      completed.map((el) => {
-        return  <CompletedTasks title={el.title} id={el.id}/>
-      })
-      : null}
+        <div className={style.completedTasks}>
+          {completed.length > 0 ? (
+            <div className={style.showCompleted}>
+              {showCompleted ? (
+                <div className={style.iconCompleted}>
+                  {" "}
+                  <IoIosArrowDown style={{ color: 'white' }} />
+                </div>
+              ) : (
+                <div className={style.iconCompleted}>
+                  {" "}
+                  <IoIosArrowForward style={{ color: 'white' }} />
+                </div>
+              )}
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  setShowCompleted(!showCompleted);
+                }}
+              >
+                {" "}
+                Completato: {completed.length}
+              </button>
+            </div>
+          ) : null}
 
+          {completed?.length > 0 &&
+          selectedCategory.title === completed[0].category && showCompleted
+            ? completed.map((el) => {
+                return <CompletedTasks title={el.title} id={el.id} />;
+              })
+            : null}
+        </div>
       </div>
-      <form className={style.addTaskBtn} onSubmit={(e => {
-        e.preventDefault();
-        handleNewTodo()
-      })} >
-        <input type="text" placeholder="Aggiungi un'attività" onChange={(e) => {
-            setNewTodoTitle(e.target.value)
-        }} ></input>
+
+      <form
+        className={style.addTaskBtn}
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleNewTodo();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Aggiungi un'attività"
+          onChange={(e) => {
+            setNewTodoTitle(e.target.value);
+          }}
+        ></input>
       </form>
     </div>
   );
